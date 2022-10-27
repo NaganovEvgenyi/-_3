0059d3749fb9ccc8cb33c5fff4b6a01dab6ab7cd
NaganovEvgenyi <enaganov@inbox.ru>
2022-10-27 20:41:01 +0300

    2 задача

--- Program.cs
+++ Program.cs
@@ -31,7 +31,7 @@ Console.WriteLine("Введите пятизначное число:");
      Console.WriteLine("Введите пятизначное число");
  } 
 
- //Математический метод*/
+ //Математический метод
 
  Console.WriteLine("Введите пятизначное число:");
  int number = int.Parse(Console.ReadLine()!);
@@ -54,4 +54,24 @@ Console.WriteLine("Введите пятизначное число:");
  else
  {
      Console.WriteLine("Введите пятизначное число!!!");
- }
+ }*/
+
+ //Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+ Console.WriteLine("Введите координату x первой точки:");
+ int x1 = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите координату y первой точки:");
+ int y1 = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите координату z первой точки:");
+ int z1 = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите координату x второй точки:");
+ int x2 = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите координату y второй точки:");
+ int y2 = int.Parse(Console.ReadLine()!);
+ Console.WriteLine("Введите координату z второй точки:");
+ int z2 = int.Parse(Console.ReadLine()!);
+
+ double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+ Console.WriteLine($"Расстояние между точками {result}");
